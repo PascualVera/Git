84131fc13ac13735ff265290febb27519e9c2e7c
PascualVera <pascualvera.994@gmail.com>
2022-01-17 13:34:15 +0100

    Funcion cuadrado añadida

---
+++ index.js
@@ -0,0 +1,22 @@
+let suma = (a, b) => {
+  return a + b;
+};
+let resta = (a, b) => {
+  return a - b;
+};
+let multiplicacion = (a, b) => {
+  return a * b;
+};
+let division = (a, b) => {
+  return a / b;
+};
+let cuadrado = (x) => {
+  return x ** 2;
+};
+module.exports = {
+  suma,
+  resta,
+  multiplicacion,
+  division,
+  cuadrado,
+};
---
+++ run.js
@@ -0,0 +1,5 @@
+let myLib = require("./index");
+console.log(myLib.suma(2, 4));
+console.log(myLib.resta(2, 4));
+console.log(myLib.multiplicacion(4, 2));
+console.log(myLib.division(2, 4));
